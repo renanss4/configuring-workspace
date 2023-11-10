6536f911d101c19b28a8b3b70a21acd11c62345d
mago <Nettoflix>
2023-11-10 17:52:52 -0300

    end

--- faslkjd.sh
+++ faslkjd.sh
@@ -4,6 +4,12 @@
 
 # Oi, bem-vindo ao meu script. Eu espero que você goste e se divirta!
 
+# Constants
+PYTHON_VERSION="3.10"
+DEADSNAKES_PPA="ppa:deadsnakes/ppa"
+VS_CODE_URL="https://go.microsoft.com/fwlink/?LinkID=760868"
+CHROME_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+
 # Function to check the system language
 check_system_language() {
     language=$(locale | grep LANGUAGE | cut -d= -f2 | cut -d: -f1)
@@ -24,12 +30,7 @@ check_system_language
 
 # Function to check if a package is installed
 check_package() {
-    if dpkg -l "$1" &> /dev/null; then
-        echo "$1 is already installed."
-        return 0
-    else
-        return 1
-    fi
+    dpkg -l "$1" &> /dev/null && { echo "$1 is already installed."; return 0; } || return 1
 }
 
 # Function to update the system
@@ -49,9 +50,7 @@ install_essential_packages() {
     packages=("git" "curl" "build-essential" "gcc" "make" "default-libmysqlclient-dev" "libssl-dev")
     
     for package in "${packages[@]}"; do
-        if ! check_package "$package"; then
-            sudo apt install "$package" -y
-        fi
+        check_package "$package" || sudo apt install "$package" -y
     done
 
     echo "Essential packages installed successfully."
@@ -59,23 +58,40 @@ install_essential_packages() {
 
 # Function to install Python
 install_python() {
-    python_version="3.10"
-    if ! check_package "python${python_version}-full" || ! check_package "python${python_version}-dev"; then
-        sudo apt install "python${python_version}-full" "python${python_version}-dev" -y
-        echo "Python ${python_version} installed successfully."
+    if [ "$(lsb_release -cs)" == "bionic" ]; then
+        echo "You are not on Ubuntu 22.04. Adding deadsnakes PPA for Python $PYTHON_VERSION..."
+        sudo add-apt-repository "$DEADSNAKES_PPA"
+        sudo apt-get update
     fi
+
+    check_package "python${PYTHON_VERSION}" || {
+        sudo apt install "python${PYTHON_VERSION}-full" -y
+        echo "Python $PYTHON_VERSION installed successfully."
+    }
+
+    check_package "python${PYTHON_VERSION}-dev" || {
+        echo "Installing Python $PYTHON_VERSION development files..."
+        sudo apt install "python${PYTHON_VERSION}-dev" -y
+        echo "Python $PYTHON_VERSION development files installed successfully."
+    }
+
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python${PYTHON_VERSION} 1
+    sudo update-alternatives --set python3 /usr/bin/python${PYTHON_VERSION}
+
+    echo "Python $PYTHON_VERSION is now the default version."
+    # Additional packages or configurations specific to Python $PYTHON_VERSION
+    # Add your custom packages or configurations here
 }
 
 # Function to install Visual Studio Code
 install_vscode() {
-    if ! check_package "code"; then
+    check_package "code" || {
         echo "Downloading and installing Visual Studio Code..."
-        wget -O /tmp/vscode.deb "https://go.microsoft.com/fwlink/?LinkID=760868"
+        wget -O /tmp/vscode.deb "$VS_CODE_URL"
         sudo dpkg -i /tmp/vscode.deb
         sudo apt install -f
         echo "Visual Studio Code installed successfully."
 
-        # Add settings to settings.json
         vscode_config="$HOME/.config/Code/User/settings.json"
         mkdir -p "$(dirname "$vscode_config")"
         cat <<EOF >"$vscode_config"
@@ -93,47 +109,47 @@ install_vscode() {
 }
 EOF
         echo "Settings added to Visual Studio Code's settings.json."
-    else
-        echo "Visual Studio Code is already installed."
-    fi
+    }
+
+    echo "Visual Studio Code is already installed."
 }
 
 # Function to install Chrome
 install_chrome() {
-    if ! check_package "google-chrome-stable"; then
+    check_package "google-chrome-stable" || {
         echo "Downloading and installing Google Chrome..."
-        wget -O /tmp/chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+        wget -O /tmp/chrome.deb "$CHROME_URL"
         sudo dpkg -i /tmp/chrome.deb
         sudo apt install -f
         echo "Google Chrome installed successfully."
-    else
-        echo "Google Chrome is already installed."
-    fi
+    }
+
+    echo "Google Chrome is already installed."
 }
 
 # Function to install Spotify
 install_spotify() {
-    if ! check_package "spotify-client"; then
+    check_package "spotify-client" || {
         echo "Downloading and installing Spotify..."
         sudo apt install snapd -y
         sudo snap install spotify
         echo "Spotify installed successfully."
-    else
-        echo "Spotify is already installed."
-    fi
+    }
+
+    echo "Spotify is already installed."
 }
 
 # Function to install Jupyter
 install_jupyter() {
-    if ! pip3 show jupyter &> /dev/null; then
+    check_package "jupyter" || {
         pip3 install jupyter
         echo "Jupyter installed successfully."
-    else
-        echo "Jupyter is already installed."
-    fi
+    }
+
+    echo "Jupyter is already installed."
 }
 
-# Function to create folders on the desktop or Área de Trabalho
+# Function to create folders on the desktop
 create_folders() {
     desktop_path="$HOME/Desktop"
     projects_path="$desktop_path/Projects"
@@ -142,27 +158,42 @@ create_folders() {
     # Check system language
     system_language=$(locale | grep LANGUAGE | cut -d= -f2 | cut -d: -f1)
 
-    if [[ "$system_language" == "pt_BR"* ]]; then
+    [[ "$system_language" == "pt_BR"* ]] && {
         desktop_path="$HOME/Área de Trabalho"
         projects_path="$desktop_path/Projects"
         workspace_path="$desktop_path/Workspace"
-    fi
+    }
 
     # Check if folders exist
-    if [ -d "$projects_path" ] && [ -d "$workspace_path" ]; then
+    [ -d "$projects_path" ] && [ -d "$workspace_path" ] && {
         echo "Folders already exist. No need to create."
-    else
-        mkdir -p "$projects_path"
-        mkdir -p "$workspace_path"
-        echo "Folders created on the desktop or Área de Trabalho."
-    fi
+    } || {
+        echo "Choose which folders to create:"
+        echo "1- Create folder Projects"
+        echo "2- Create folder Workspace"
+        echo "3- Create both folders"
+
+        read -rp "Enter the numbers corresponding to your desired options: " choices
+
+        # Create an array of choices
+        choices_array=($(echo "$choices" | sed 's/\(.\)/\1 /g'))
+
+        for choice in "${choices_array[@]}"; do
+            case $choice in
+                1) mkdir -p "$projects_path" && echo "Folder Projects created." ;;
+                2) mkdir -p "$workspace_path" && echo "Folder Workspace created." ;;
+                3) mkdir -p "$projects_path" "$workspace_path" && echo "Both folders created." ;;
+                *) echo "Invalid choice: $choice. Skipping." ;;
+            esac
+        done
+    }
 }
 
 # Function to print Hello World in binary ASCII art
 print_hello_world() {
     cat << "EOF"
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -    
-- 01001000 01100101 01101100 01101100 01101111              -
+- 01001000 01100101 01101100 01101111              -
 - 01010111 01101111 01110010 01101100 01100100 00100001     -
 -  __  __          ___    ___                               -
 - /\ \/\ \        /\_ \  /\_ \                              -
@@ -199,6 +230,7 @@ choose_installations() {
     print_hello_world
     ask_sudo_password
     read -rp "Enter your name: " username
+    echo ""
     echo "Hello $username! How would you like to configure your environment?"
     echo "1- Install Everything"
     echo "2- Essential Packages"
@@ -207,13 +239,13 @@ choose_installations() {
     echo "5- Visual Studio Code"
     echo "6- Spotify"
     echo "7- Google Chrome"
-    echo "8- Do not install anything"
+    echo "8- Create folders in Desktop"
+    echo "9- Do not install anything"
 
-    read -rp "Enter the number corresponding to your desired option: " choice
+    read -rp "Enter the numbers corresponding to your desired options (e.g., 27 for Essential Packages and Google Chrome): " choices
 
-    # Remove spaces and hyphens, if any, and convert the string to an array
-    choices=$(echo "$choices" | tr -d ' -')
-    choices_array=($(echo $choices | sed 's/\(.\)/\1 /g'))
+    # Create an array of choices
+    choices_array=($(echo "$choices" | sed 's/\(.\)/\1 /g'))
 
     for choice in "${choices_array[@]}"; do
         case $choice in
@@ -224,7 +256,8 @@ choose_installations() {
             5) install_vscode ;;
             6) install_spotify ;;
             7) install_chrome ;;
-            8) echo "Nothing will be installed. Exiting." ;;
+            8) create_folders ;;
+            9) echo "Nothing will be installed. Exiting." ;;
             *) echo "Invalid choice: $choice. Skipping." ;;
         esac
     done
